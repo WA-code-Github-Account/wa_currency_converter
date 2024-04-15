798f3701dcaf8cad3d83d221e18eab9f1a2ba48e
WA Siddiqui-coder <163776495+WA-code-Github-Account@users.noreply.github.com>
2024-04-14 17:38:57 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let c_value = {
+    USD: 1,
+    UAE: 3.67,
+    PKR: 278.40,
+};
+let ans = await inquirer.prompt([{
+        name: "from",
+        type: "list",
+        message: chalk.yellow.bold("\n\nconverting currency from:"),
+        choices: ["USD", "UAE", "PKR"],
+    },
+    { name: "to",
+        type: "list",
+        message: chalk.green.bold("\nconverting currency to:\n"),
+        choices: ["USD", "UAE", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: chalk.red.bold("\nEnter the amount:\n"),
+    },
+]);
+console.log(chalk.blue.bold(c_value[ans.to] / c_value[ans.from] * ans.amount));
